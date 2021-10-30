873e3a95ca7efea002521b9018e2580b973764c7
spomni <mikhail.snitko@gmail.com>
2021-10-30 16:23:26 +0300

    Remove the "output.clean" webpack config property.

--- test/boilerplate/webpack.config.js
+++ test/boilerplate/webpack.config.js
@@ -4,9 +4,6 @@ const path = require('path')
 const config = {
   mode: 'development',
   entry: './src/main.js',
-  output: {
-    clean: true,
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, './src/index.html')
